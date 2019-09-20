66a9cbf4d4e2122c9260d84acdb713467b8ddd80
Anthony Loffredo <loffredo.ap@gmail.com>
2019-09-20 10:09:16 -0700

    Adding generic message interface for type safety
    Adding generic message interface for type safety

--- Interfaces/IMessageHandler.cs
+++ Interfaces/IMessageHandler.cs
@@ -1,8 +1,12 @@
-﻿namespace Penguin.Messaging.Abstractions.Interfaces
+﻿using System;
+
+namespace Penguin.Messaging.Abstractions.Interfaces
 {
     /// <summary>
     /// This class denotes a class set to be automatically subscribed by the message handler when the SubscribeAll method is called
     /// </summary>
+    [Obsolete("This non-generic interface should be avoided in favor of the generic version that forces better type safety", false)]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1040:Avoid empty interfaces", Justification = "<Pending>")]
     public interface IMessageHandler
     {
     }
---
+++ Interfaces/IMessageHandlerT.cs
@@ -0,0 +1,31 @@
+﻿namespace Penguin.Messaging.Abstractions.Interfaces
+{
+#pragma warning disable CS0618 // Type or member is obsolete
+    /// <summary>
+    /// This class denotes a class set to be automatically subscribed by the message handler when the SubscribeAll method is called
+    /// </summary>
+    public interface IMessageHandler<T> : IMessageHandler
+#pragma warning restore CS0618 // Type or member is obsolete
+    {
+        /// <summary>
+        /// Denotes that this method is used to accept messages from the message bus
+        /// </summary>
+        /// <param name="message">The message to accept</param>
+        void AcceptMessage(T message);
+    }
+
+#pragma warning disable CS0618 // Type or member is obsolete
+                              /// <summary>
+                              /// This class denotes a class set to be automatically subscribed by the message handler when the SubscribeAll method is called
+                              /// </summary>
+    public interface IMessageHandler<T1, T2> : IMessageHandler
+#pragma warning restore CS0618 // Type or member is obsolete
+    {
+        /// <summary>
+        /// Denotes that this method is used to accept messages from the message bus
+        /// </summary>
+        /// <param name="message">The message to accept</param>
+        /// <param name="parameter">An additional object that is required along with the message</param>
+        void AcceptMessage(T1 message, T2 parameter);
+    }
+}
