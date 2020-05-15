827ae9d49974bfc70650de461678c5e7d2c37fff
anloffredo <anloffredo@massageenvy.com>
2020-05-14 19:36:26 -0700

    Auto Commit

--- Interfaces/IMessage.cs
+++ Interfaces/IMessage.cs
@@ -1,5 +1,6 @@
 ﻿namespace Penguin.Messaging.Abstractions.Interfaces
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1040:Avoid empty interfaces", Justification = "<Pending>")]
     public interface IMessage
     {
     }
