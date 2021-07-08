a84a7bebc50f3fcded99025b49a9c23dca9882ed
AnLoffredo <38223101+AnLoffredo@users.noreply.github.com>
2021-07-08 14:53:42 -0700

    Autocommit
    Autocommit

--- Interfaces/IMessageHandler.cs
+++ Interfaces/IMessageHandler.cs
@@ -6,7 +6,6 @@ namespace Penguin.Messaging.Abstractions.Interfaces
     /// This class denotes a class set to be automatically subscribed by the message handler when the SubscribeAll method is called
     /// </summary>
     [Obsolete("This non-generic interface should be avoided in favor of the generic version that forces better type safety", false)]
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1040:Avoid empty interfaces", Justification = "<Pending>")]
     public interface IMessageHandler
     {
     }
